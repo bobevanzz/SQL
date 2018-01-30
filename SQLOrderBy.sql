bb26dcfbbb67b352b8e57af5f1a058488aad6b3f
Bob Evans <bedrockballer@gmail.com>
2018-01-30 15:52:32 -0500

    added more examples to OrderBy

--- SQLOrderBy.sql
+++ SQLOrderBy.sql
@@ -6,3 +6,16 @@ select name , IsCorpAcct, City
 from customer 
 where IsCorpAcct =1
 order by city
+--list orders where customers are from ohio
+select * from 
+[customer] c
+join [order] o
+on c.id = o.CustomerId
+where [state] = 'OH'
+--all totals of orders summarized by state
+select state, sum(amount) 
+from customer c
+join [order] o
+on o.CustomerId = c.Id
+group by c.State
+having c.state in ('KY', 'IN')
