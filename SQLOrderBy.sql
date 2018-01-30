15c91134a38aeeeea1d9ade260908ad31be4319e
Bob Evans <bedrockballer@gmail.com>
2018-01-30 15:29:33 -0500

    added edit to OrderBy

--- SQLOrderBy.sql
+++ SQLOrderBy.sql
@@ -1,5 +1,6 @@
 -- list customer where creditlimit between 200000 and 500000 inclusive
-
+select name, creditlimit from customer
+where creditlimit >= 200000 and creditlimit<= 500000
 -- list customers where IsCorpAcct is 1 and sort by City
 select name , IsCorpAcct, City
 from customer 
